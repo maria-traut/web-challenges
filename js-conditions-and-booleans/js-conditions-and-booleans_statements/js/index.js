b9f6a3a3b83b1d2cd2cc922b4e433103668dd707
Maria <mia.gotcha@proton.me>
2026-03-19 17:00:35 +0100

    change smth

--- js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -2,19 +2,42 @@ console.clear();
 
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
-
 const receivedPassword = "password1234";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Access denied!");
+} else {
+  console.log("Welcome! Your are logged in as Brunhilede1984.");
+}
 
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
+
+if (numberOfHotdogs < 5) {
+  console.log("It's 2 € per hotdog.");
+} else if (numberOfHotdogs >= 5 && numberOfHotdogs < 100) {
+  console.log("It's 1.50 € per hotdog.");
+} else if (numberOfHotdogs >= 100 && numberOfHotdogs < 1000000) {
+  console.log("It's 1 € per hotdog.");
+} else if (numberOfHotdogs >= 1000000) {
+  console.log("It's 0.10 € per hotdog.");
+} else {
+  console.log("damn");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn ..." : "Partytime!!!";
 
 console.log(statement);
 
