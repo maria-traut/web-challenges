93e26b29cc033af4591b72badbc0978b80b88779
Maria <mia.gotcha@proton.me>
2026-03-23 15:55:13 +0100

    try multiple-inputs challenge

--- js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -5,36 +5,45 @@ const secondCopyInput = document.querySelector("[data-js=second-copy-input]");
 const copyButton = document.querySelector("[data-js=copy-button]");
 
 // --v-- Write your code here --v--
-
+copyButton.addEventListener("click", () => {
+  secondCopyInput.value = firstCopyInput.value;
+});
 // --^-- End of Task 1 --^--
 
 // Task 2: Copy and Uppercase
 
 const firstCopyInUppercaseInput = document.querySelector(
-  "[data-js=first-copy-uppercase-input]"
+  "[data-js=first-copy-uppercase-input]",
 );
 const secondCopyInUppercaseInput = document.querySelector(
-  "[data-js=second-copy-uppercase-input]"
+  "[data-js=second-copy-uppercase-input]",
 );
 const copyInUppercaseButton = document.querySelector(
-  "[data-js=copy-uppercase-button]"
+  "[data-js=copy-uppercase-button]",
 );
 
 // --v-- Write your code here --v--
-
+copyInUppercaseButton.addEventListener("click", () => {
+  secondCopyInUppercaseInput.value =
+    firstCopyInUppercaseInput.value.toUpperCase();
+});
 // --^-- End of Task 2 --^--
 
 // Task 3: Switch Values
 
 const firstSwitchValueInput = document.querySelector(
-  "[data-js=first-switch-value-input]"
+  "[data-js=first-switch-value-input]",
 );
 const secondSwitchValueInput = document.querySelector(
-  "[data-js=second-switch-value-input]"
+  "[data-js=second-switch-value-input]",
 );
 const switchValueButton = document.querySelector(
-  "[data-js=switch-value-button]"
+  "[data-js=switch-value-button]",
 );
+
+switchValueButton.addEventListener("click", () => {
+  secondSwitchValueInput.value = secondSwitchValueInput.toggleAttribute;
+});
 
 // --v-- Write your code here --v--
 
