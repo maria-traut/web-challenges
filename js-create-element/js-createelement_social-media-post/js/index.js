a75bda9dc6e112dc82403e1ade6060d049f928eb
Maria <mia.gotcha@proton.me>
2026-03-25 13:26:27 +0100

    solve js-create-element challenges

--- js-create-element/js-createelement_social-media-post/js/index.js
+++ js-create-element/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,53 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+/*
+-------first exercixe-------
+
+const newPost = document.createElement("post");
+newPost.classList.add("post");
+newPost.textContent = "I'm another toast social media post.";
+body.append(newPost);
+
+const newPostContent = document.createElement("post__content");
+newPostContent.classList.add("post__content");
+article.append(newPostContent);
+
+const newPostFooter = document.createElement("post__footer");
+newPostFooter.classList.add("post__footer");
+article.append(newPostFooter);
+
+const newPostUsername = document.createElement("post__username");
+newPostUsername.classList.add("post__username");
+article.append(newPostUsername);
+
+const newButton = document.createElement("post__button");
+newButton.classList.add("post__button");
+article.append(newButton);
+*/
+
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+document.body.append(newPost);
+
+const newPostContent = document.createElement("p");
+newPostContent.classList.add("post__content");
+newPostContent.textContent =
+  "Another social media post, reproduced by DOM manipulation in JavaScript.";
+newPost.append(newPostContent);
+
+const newPostFooter = document.createElement("footer");
+newPostFooter.classList.add("post__footer");
+newPost.append(newPostFooter);
+
+const newPostUsername = document.createElement("span");
+newPostUsername.classList.add("post__username");
+newPostUsername.textContent = "@anotherusername";
+newPostFooter.append(newPostUsername);
+
+const newButton = document.createElement("button");
+newButton.classList.add("post__button");
+newButton.textContent = "♥♥ Like";
+newPostFooter.append(newButton);
+
+newButton.addEventListener("click", handleLikeButtonClick);
--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,14 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const newToastMessage = document.createElement("li");
+  newToastMessage.classList.add("toast-container__message");
+  newToastMessage.textContent = "I'm another toast message.";
+  toastContainer.append(newToastMessage);
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
