2e66cb617e9b4a94ffaffc2f3f7073505fc4a59e
Maria <mia.gotcha@proton.me>
2026-03-31 16:22:03 +0200

    solve star wars cards challenge

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,12 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  const people = data.results;
+  people.forEach((person) => {
+    const card = Card(person);
+    renderElement(card);
+  });
 }
