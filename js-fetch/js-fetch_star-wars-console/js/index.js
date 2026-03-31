2cc120ef60867c2ada482510e9a2bd511319d6f0
Maria <mia.gotcha@proton.me>
2026-03-31 15:31:41 +0200

    solve star wars console challenge

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,24 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log("data: ", data);
+  console.log("data.results: ", data.results);
+
+  const firstPerson = data.results[0];
+  console.log("firstPerson: ", firstPerson);
+  console.log("firstPerson.name: ", firstPerson.name);
+  console.log("firstPerson.eye_color: ", firstPerson.eye_color);
+}
 
 fetchData();
+
+/* Bonus
+
+  const eyeR2D2 = data.results.find((person) => person.name === "R2-D2");
+  console.log("R2-D2s eye color: ", eyeR2D2.eye_color);
+  
+*/
