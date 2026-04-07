4e840dbbf2cef15807e067f9296e3e063dbd4e6f
Maria <mia.gotcha@proton.me>
2026-04-07 13:12:06 +0200

    solve challenges

--- js-modern-syntax/js-modern-syntax_destructuring-objects/js/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-objects/js/index.js
@@ -12,7 +12,7 @@ const company = {
   location: "Hamburg",
 };
 
-// Example: export const { value1 } = myObject;
+export const { course } = company;
 
 /*
 EXERCISE 2
@@ -26,6 +26,8 @@ Use destructuring to extract the following:
 
 const user = { name: "John", years: 30 };
 
+export const { name, years: age, isAdmin = false } = user;
+
 /*
 EXERCISE 3
 Extract properties from the dog object and
@@ -37,6 +39,8 @@ const dog = {
   breed: "husky",
   age: 5,
 };
+
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
 
 /*
 EXERCISE 4
@@ -50,6 +54,8 @@ const person = {
   firstName: "Alex",
 };
 
+export const { lastName: personLastName, ...moreInformation } = person;
+
 /*
 EXERCISE 5
 Refactor the following function to use destructuring assignment for the
@@ -58,14 +64,11 @@ Hint: You may need to rename one property during destructuring.
 */
 
 export function logInfo(city) {
-  const name = city.name;
-  const country = city.country;
-  const numPeople = city.population;
-
+  const { name, country, population: numPeople } = city;
   return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
 }
 
 // Usage example:
 console.log(
-  logInfo({ name: "Marseille", country: "France", population: 861635 })
+  logInfo({ name: "Marseille", country: "France", population: 861635 }),
 );
