ae9b8a7550ac5ecd7930f12537226fdbad771ca8
Maria <mia.gotcha@proton.me>
2026-04-10 15:16:53 +0200

    solve hello-world-article-challenge

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,16 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>React Basics</h1>
+      <p>This is the first p tag content.</p>
+      <p>This is the second p tag content.</p>
+      <button>Show energy level</button>
+    </article>
+  );
 }
