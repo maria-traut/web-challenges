a53c5ff4db665ad3ec19232915a7a7235eaa9480
Maria <mia.gotcha@proton.me>
2026-04-13 15:29:39 +0200

    solve buttons-and-children challenge

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>hi, i'm button no 1</Button>
+      <Button>hello, my dear</Button>
+      <Button>hola chico</Button>
+      <Button>bonjour, madame</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
