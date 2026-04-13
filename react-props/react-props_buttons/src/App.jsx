3f40b99cf826a0d4d5b3eaf891433bc9ecd5dcf3
Maria <mia.gotcha@proton.me>
2026-04-13 12:25:32 +0200

    start react-props-buttons challenge

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,44 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button
+        color="salmon"
+        text="i am a salmon colored button"
+        onClick={handleClick}
+      />
+      <Button
+        disabled
+        color="darkblue"
+        text="i am a darkblue button"
+        onClick={handleClick}
+      />
+      <Button
+        color="hotpink"
+        text="i am a hotpink button"
+        onClick={handleClick}
+      />
+      <Button
+        backgroundColor="black"
+        color="lightblue"
+        text="i am a lightblue button"
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
+
+function Button({ color, backgroundColor, disabled, text, onClick }) {
+  function handleClick(text) {
+    console.log("You clicked me!");
+  }
+  return (
+    <button
+      onClick={() => alert(`You clicked me!`)}
+      type="button"
+      disabled={disabled}
+      style={{ color: color, backgroundColor: backgroundColor }}
+    >
+      {text}
+    </button>
+  );
 }
