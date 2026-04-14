e0919e237055e279a70e90bd151b3ce33bc15fce
Maria <mia.gotcha@proton.me>
2026-04-14 16:45:45 +0200

    change some challenge elements

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,20 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 0, name: "🍌 banana", color: "yellow" },
+    { id: 2, name: "🍓 strawberry", color: "salmon" },
+    { id: 1, name: "🍍 pineapple", color: "yellow" },
+    { id: 3, name: "🫐 blueberry", color: "violet" },
+    { id: 4, name: "🍐 pear", color: "lightyellow" },
+    { id: 5, name: "🍉 watermelon", color: "lightgreen" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruits.id} color={fruit.color} name={fruit.name} />
+      ))}
     </div>
   );
 }
--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,20 +1,11 @@
 import "./Card.css";
 
-export default function Card({ id, name }) {
-  const fruits = [
-    { id: 1, name: "🍍 pineapple", color: "yellow" },
-    { id: 2, name: "🍓 strawberry", color: "red" },
-    { id: 3, name: "🫐 blueberry", color: "purple" },
-    { id: 4, name: "🍐 pear", color: "yellow" },
-    { id: 5, name: "🍉 watermelon", color: "green" },
-  ];
+import "./Card.css";
+
+export default function Card({ name, color }) {
   return (
-    <p className="card">
-      {fruits.map((fruit) => (
-        <li key={fruit.id} color={fruit.color}>
-          {fruit.name}
-        </li>
-      ))}
+    <p className="card" style={{ backgroundColor: color }}>
+      {name}
     </p>
   );
 }
