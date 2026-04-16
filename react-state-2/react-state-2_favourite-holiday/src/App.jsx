a013276d1b37374c4141f31d863f302c6a6bf926
Maria <mia.gotcha@proton.me>
2026-04-16 13:39:33 +0200

    solve react state 2 favourite holiday challenge

--- react-state-2/react-state-2_favourite-holiday/src/App.jsx
+++ react-state-2/react-state-2_favourite-holiday/src/App.jsx
@@ -1,8 +1,18 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [holiday, setHoliday] = useState(" ");
+  const [date, setDate] = useState(" ");
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    const form = event.target;
+    const holiday = form.elements.holiday.value;
+    const date = form.elements.date.value;
+    setHoliday(holiday);
+    setDate(date);
   }
 
   return (
@@ -29,10 +39,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
