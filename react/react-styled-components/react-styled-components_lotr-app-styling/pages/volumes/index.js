1c389db9d3e55eb8217859ca66b2a2f3a82f0f81
Maria <mia.gotcha@proton.me>
2026-04-29 11:24:18 +0200

    add styling except for dark mode

--- react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
+++ react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
@@ -1,18 +1,70 @@
 import Link from "next/link";
+import Image from "next/image";
 import { introduction, volumes } from "../../lib/data";
+import styled from "styled-components";
 
 export default function Volumes() {
   return (
-    <>
-      <h1>The Lord of the Rings</h1>
-      <p>{introduction}</p>
-      <ul>
+    <Main>
+      <Title>The Lord of the Rings</Title>
+      <Introduction>{introduction}</Introduction>
+      <VolumesOverview>All Volumes</VolumesOverview>
+      <BookList>
         {volumes.map((volume) => (
-          <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
-          </li>
+          <BookTitle key={volume.id}>
+            <Image
+              src={volume.cover}
+              alt={`Cover image of ${volume.title}`}
+              width={80}
+              height={130}
+            />
+            <StyledLink href={`/volumes/${volume.slug}`}>
+              {volume.title}
+            </StyledLink>
+          </BookTitle>
         ))}
-      </ul>
-    </>
+      </BookList>
+    </Main>
   );
 }
+const Main = styled.main`
+  padding: 2.5rem;
+`;
+
+const Title = styled.h1`
+  font: var(--font-headline-1);
+`;
+const Introduction = styled.p`
+  font: var(--font-body);
+  padding-block: 1rem;
+`;
+
+const VolumesOverview = styled.h2`
+  font: var(--font-headline-2);
+`;
+
+const BookList = styled.ul`
+  list-style: none;
+  display: grid;
+  padding: 0;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+  justify-items: center;
+`;
+
+const BookTitle = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 80px;
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+  font: var(--font-caption);
+  &:hover {
+    text-decoration: underline;
+    color: salmon;
+  }
+`;
