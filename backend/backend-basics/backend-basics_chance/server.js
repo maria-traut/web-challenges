965562b6499df445e40e1495002561bcea9a8fb8
Maria <mia.gotcha@proton.me>
2026-05-11 16:06:47 +0200

    solve third challenge

--- backend/backend-basics/backend-basics_chance/server.js
+++ backend/backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`,
+  );
+});
+
+const chance = new Chance();
