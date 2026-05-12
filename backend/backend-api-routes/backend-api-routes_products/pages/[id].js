4030737353284169c2135e6a01d7b80d08506167
Maria <mia.gotcha@proton.me>
2026-05-12 14:14:58 +0200

    change folder structure

--- backend/backend-api-routes/backend-api-routes_products/pages/[id].js
+++ backend/backend-api-routes/backend-api-routes_products/pages/[id].js
@@ -5,8 +5,8 @@ const fetcher = (...args) => fetch(...args).then((response) => response.json());
 export default function DetailsPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { data: product } = useSWR(`api/products/${id}`, fetcher);
-  console.log("product", product);
+  const { data: product } = useSWR(id ? `api/products/${id}` : null, fetcher);
+
   if (!product) return <h1>Loading ...</h1>;
   return <p>{product.name}</p>;
 }
